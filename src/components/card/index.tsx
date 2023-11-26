ff6ff8e88b8f388c8a43fa644e65f114f9f2d10b
KunalModi117 <kunalmodi117@gmail.com>
2023-11-26 16:28:03 +0530

    minor changes

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,7 +1,7 @@
 import PersonImage from '../../assets/Person.svg'
 export const Card = ({ imgSrc, smallHeading, mainHeading, capacity, desc, price, denominator }: any) => {
     return (
-        <div className="flex flex-col h-fit w-fit justify-center items-center bg-slate-50 rounded-[42px] shadow-2xl p-5 gap-4 pb-10 cursor-pointer">
+        <div className="flex flex-col h-fit w-fit justify-center items-center bg-slate-50 rounded-[42px] shadow-2xl p-5 gap-4 pb-10 cursor-pointer hover:animate-pulse">
             <img src={imgSrc} alt="" className="w-80 h-64 object-cover rounded-3xl" />
             <div className="w-full text-xs font-bold text-gray-700">{smallHeading}</div>
             <div className="flex justify-between w-full font-bold"><div className='text-2xl'>{mainHeading}</div> <div className="flex"><img src={PersonImage} alt="" className="mr-2 mt-1" /><div className="font-semibold flex items-end">{capacity}</div></div></div>
--- src/components/roundedImage/index.tsx
+++ src/components/roundedImage/index.tsx
@@ -1,6 +1,6 @@
 export const RoundedImage = ({ img, name }: { img: string, name?: string }) => {
     return (
-        <div className="relative cursor-pointer">
+        <div className="relative cursor-pointer hover:animate-pulse">
             <img src={img} alt="VerticalImage" className="rounded-[42px] w-full h-full object-cover" />
             <div className="absolute bottom-6 font-bold text-2xl text-white w-full flex justify-center">
                 {name ? name : ""}
